aeb5d0df4e4045800c21bf4eaffb134075329415
topkoong <engineer_top@msn.com>
2017-02-14 01:33:37 -0500

    Add files via upload

---
+++ Whats the Weather/Whats the Weather/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  Whats the Weather
+//
+//  Created by toppy on 10/9/16.
+//  Copyright © 2016 theeruttop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var cityTextField: UITextField!
+
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
+    @IBAction func getWeather(_ sender: AnyObject) {
+        
+        if let url = URL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.replacingOccurrences(of: " ", with: "-") + "/forecasts/latest") {
+            
+            let request = NSMutableURLRequest(url: url)
+        
+            // cast to URL Request
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+            
+                var message = ""
+            
+                if error != nil {
+                
+                    print(error)
+                }
+                else {
+                
+                    if let unwrappedData = data {
+                    
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    
+                        var stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                    
+                        if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                        
+                            if contentArray.count > 1 {
+                            
+                                stringSeparator = "</span>"
+                            
+                                let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
+                            
+                                if newContentArray.count > 1 {
+                                
+                                    message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                
+                                    print(message)
+                                
+                                }
+                            
+                            }
+                        
+                        }
+                    
+                    }
+                
+                    if message == "" {
+                    
+                        message = "The weather couldn't be found. Please try again"
+                    }
+                
+                    DispatchQueue.main.sync(execute: {
+                    
+                        self.resultLabel.text = message // Use self to reference the view controller
+                    
+                    })
+                
+                }
+            
+            
+            
+            }
+        
+            task.resume()
+        
+        } else {
+    
+        resultLabel.text = "The weather couldn't be found. Please try again"
+    
+        }
+ 
+    
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
